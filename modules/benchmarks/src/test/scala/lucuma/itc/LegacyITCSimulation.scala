6aac9151e75601976e6bf5ce3cca57ec5e31a1c6
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2021-11-18 15:47:45 -0300

    Add Legacy ITC test
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

---
+++ modules/benchmarks/src/test/scala/lucuma/itc/LegacyITCSimulation.scala
@@ -0,0 +1,111 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.itc
+
+// import scala.concurrent.duration._
+//
+import cats.syntax.all._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.circe.Json
+import io.gatling.http.funspec.GatlingHttpFunSpec
+import lucuma.itc.ItcSourceDefinition._
+import lucuma.core.model.SpatialProfile
+import io.circe.syntax._
+import lucuma.core.model.SpectralDistribution
+import eu.timepit.refined.numeric.Positive
+
+import coulomb.refined._
+import coulomb.si.Kelvin
+import lucuma.core.enum.MagnitudeSystem
+import lucuma.core.math.Redshift
+import lucuma.core.math.MagnitudeValue
+import lucuma.core.enum.MagnitudeBand
+import lucuma.core.math.Angle
+import lucuma.itc.search.ObservingMode
+import lucuma.core.math.Wavelength
+import lucuma.core.enum.GmosNorthDisperser
+import lucuma.core.enum.GmosNorthFpu
+import lucuma.core.enum.GmosNorthFilter
+import lucuma.core.enum.ImageQuality
+import lucuma.core.enum.CloudExtinction
+import lucuma.core.enum.WaterVapor
+import lucuma.core.enum.SkyBackground
+import lucuma.core.util.Enumerated
+
+class LegacyITCSimulation extends GatlingHttpFunSpec {
+  val headers_10 = Map("Content-Type" -> """application/json""")
+  val baseUrl    = "https://gemini-new-itc.herokuapp.com"
+
+  val sourceDefinition = ItcSourceDefinition(
+    SpatialProfile.PointSource,
+    SpectralDistribution.BlackBody(BigDecimal(50.1).withRefinedUnit[Positive, Kelvin]),
+    MagnitudeValue(5),
+    MagnitudeSystem.Jy.asLeft,
+    MagnitudeBand.Ap,
+    Redshift(0.1)
+  )
+  val obs              = ItcObservationDetails(
+    calculationMethod = ItcObservationDetails.CalculationMethod.SignalToNoise.Spectroscopy(
+      exposures = 1,
+      coadds = None,
+      exposureDuration = 1,
+      sourceFraction = 1.0,
+      ditherOffset = Angle.Angle0
+    ),
+    analysisMethod = ItcObservationDetails.AnalysisMethod.Aperture.Auto(5)
+  )
+
+  val telescope                    = ItcTelescopeDetails(
+    wfs = ItcWavefrontSensor.OIWFS
+  )
+  val instrument                   = ItcInstrumentDetails.fromObservingMode(
+    ObservingMode.Spectroscopy.GmosNorth(Wavelength.decimalNanometers.getOption(60).get,
+                                         GmosNorthDisperser.B1200_G5301,
+                                         GmosNorthFpu.Ifu2Slits,
+                                         GmosNorthFilter.GG455.some
+    )
+  )
+  def conditions(iq: ImageQuality) =
+    ItcObservingConditions(iq, CloudExtinction.OnePointFive, WaterVapor.Dry, SkyBackground.Dark, 2)
+
+  def body(c: ItcObservingConditions) = Json.obj(
+    "source"      -> sourceDefinition.asJson,
+    "observation" -> obs.asJson,
+    "conditions"  -> c.asJson,
+    "telescope"   -> telescope.asJson,
+    "instrument"  -> instrument.asJson
+  )
+
+  val allConditions =
+    Enumerated[ImageQuality].all
+      .zip(Enumerated[CloudExtinction].all)
+      .zip(Enumerated[WaterVapor].all)
+      .zip(Enumerated[SkyBackground].all)
+      .map { case (((iq, ce), wv), sb) =>
+        ItcObservingConditions(iq, ce, wv, sb, 2)
+      }
+  // val allConditions  = Enumerated[ImageQuality].all.map { iq =>
+  //   Enumerated[CloudExtinction].all.map { ce =>
+  //     Enumerated[WaterVapor].all.map { wv =>
+  //       Enumerated[SkyBackground].all.map { sb =>
+  //         ItcObservingConditions(iq, ce, wv, sb, 2)
+  //       }
+  //     }.flatten
+  //   }.flatten
+  // }.flatten
+  println(allConditions.length)
+
+  allConditions.map { c =>
+    spec {
+      http("sanity")
+        .post("/json")
+        .headers(headers_10)
+        .check(status.in(200, 400))
+        .check(substring("decode").notExists)
+        .body(StringBody(body(c).noSpaces))
+    }
+  }
+
+}
